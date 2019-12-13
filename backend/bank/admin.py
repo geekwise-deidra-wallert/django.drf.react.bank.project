af992f157db8cfb8ba75e1507487895e3c6841ec
deidra <deidraann22@gmail.com>
2019-12-13 11:12:49 -0800

    update: forgot to add account and product to admin.py

--- backend/bank/admin.py
+++ backend/bank/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Branch, Client
+from .models import Branch, Client, Product, Account
 
 admin.site.register(Branch)
 admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Account)
 # admin.site.register(Account)
