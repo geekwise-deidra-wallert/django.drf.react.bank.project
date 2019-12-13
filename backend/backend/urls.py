d7cdda0219ca71ac911520c67945256cae658781
deidra <deidraann22@gmail.com>
2019-12-13 10:09:22 -0800

    Update: added serializers and configed settings.py views.py and urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from todo import views 
+from todo import views
+from bank import views as bank_views
 
 router = routers.DefaultRouter() 
 router.register(r'todos', views.TodoView, 'todo')
+router.register(r'users', bank_views.UserViewSet)
+router.register(r'groups', bank_views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- backend/bank/views.py
+++ backend/bank/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from bank.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class UserViewSet ( viewsets.ReadOnlyModelViewSet ):
+    """
+        API endpoint that allows users to be viewed or edited
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet ( viewsets.ReadOnlyModelViewSet ):
+    """
+        API endpoint that allows users to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
