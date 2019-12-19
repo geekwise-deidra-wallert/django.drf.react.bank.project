c1639c8a9102a68e40b267415a62902da09a562d
deidra <deidraann22@gmail.com>
2019-12-19 11:01:18 -0800

    bugfix: working on getting react to display accounts from drf

--- local-front/src/bankApp.js
+++ local-front/src/bankApp.js
@@ -1,58 +1,58 @@
-import React, { Component } from "react";
-// import Modal from "./components/Modal";
-import axios from "axios";
+// import React, { Component } from "react";
+// // import Modal from "./components/Modal";
+// import axios from "axios";
 
-class App extends Component {
-    constructor ( props ) {
-        super ( props );
+// class App extends Component {
+//     constructor ( props ) {
+//         super ( props );
 
-        this.state = {
-            accountInfo: {
-                id: "",
-                account_holder: ""
-            },
-            listAccounts: []
-        };
-    }
+//         this.state = {
+//             accountInfo: {
+//                 id: 2,
+//                 account_holder: "De"
+//             },
+//             listAccounts: []
+//         };
+//     }
 
-    componentDidMount () {
-        this.refreshList ();
-    }
+//     componentDidMount () {
+//         this.refreshList ();
+//     }
 
-    refreshList = () => {
-        axios
-            .get("https://bank-backend-deidra.herokuapp.com/account/")
-            .then(Response => this.setState({listAccounts: Response.data}))
-            .catch(error => console.log(error));
-    };
+//     refreshList = () => {
+//         axios
+//             .get("https://bank-backend-deidra.herokuapp.com/account/")
+//             .then(Response => this.setState({listAccounts: Response.data.results}))
+//             .catch(error => console.log(error));
+//     };
 
-    renderAccounts = () => {
-        const accounts = this.state.listAccounts;
-        return accounts.map(account => (
-            <li key={account.id} className="list-group-item d-flex justify-content-between align-items-center">
-                <span className={`todo-title mr-2`}>
-                    {account.account_name}
-                </span>
-            </li>
-        ));
-    };
+//     renderAccounts = () => {
+//         const accounts = this.state.listAccounts;
+//         return this.accounts
+         
+//             // <li key={account.id} className="list-group-item d-flex justify-content-between align-items-center">
+//             //     <span className={`todo-title mr-2`}>
+//             //         {account.account_name}
+//             //     </span>
+//             // </li>
+//     };
 
-    render () {
-        return (
-            <main className="content">
-                <h1 className="text-white text-uppercase text-center my-4">Bank App</h1>
-                <div className="row ">
-                    <div className="col-md-6 col-sm-10 mx-auto p-0">
-                        <div className="card p-3">
-                            <ul className="list-group list-group-flush">
-                                {this.renderAccounts()}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </main> 
-        );
-    }
+//     render () {
+//         return (
+//             <main className="content">
+//                 <h1 className="text-white text-uppercase text-center my-4">Bank App</h1>
+//                 <div className="row ">
+//                     <div className="col-md-6 col-sm-10 mx-auto p-0">
+//                         <div className="card p-3">
+//                             <ul className="list-group list-group-flush">
+//                                 {this.renderAccounts()}
+//                             </ul>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </main> 
+//         );
+//     }
 
-}
-export default App;
+// }
+// export default App;
--- local-front/src/index.js
+++ local-front/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';       // add this
 import './index.css';
-import App from './bankApp';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
