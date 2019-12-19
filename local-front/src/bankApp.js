16758d7dcc6d5ddfc604f99212d25fa5c5286504
deidra <deidraann22@gmail.com>
2019-12-18 16:30:42 -0800

    working on frontend encountering some errrors

---
+++ local-front/src/bankApp.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+// import Modal from "./components/Modal";
+import axios from "axios";
+
+class App extends Component {
+    constructor ( props ) {
+        super ( props );
+
+        this.state = {
+            accountInfo: {
+                id: "",
+                account_holder: ""
+            },
+            listAccounts: []
+        };
+    }
+
+    componentDidMount () {
+        this.refreshList ();
+    }
+
+    refreshList = () => {
+        axios
+            .get("https://bank-backend-deidra.herokuapp.com/account/")
+            .then(Response => this.setState({listAccounts: Response.data}))
+            .catch(error => console.log(error));
+    };
+
+    renderAccounts = () => {
+        const accounts = this.state.listAccounts;
+        return accounts.map(account => (
+            <li key={account.id} className="list-group-item d-flex justify-content-between align-items-center">
+                <span className={`todo-title mr-2`}>
+                    {account.account_name}
+                </span>
+            </li>
+        ));
+    };
+
+    render () {
+        return (
+            <main className="content">
+                <h1 className="text-white text-uppercase text-center my-4">Bank App</h1>
+                <div className="row ">
+                    <div className="col-md-6 col-sm-10 mx-auto p-0">
+                        <div className="card p-3">
+                            <ul className="list-group list-group-flush">
+                                {this.renderAccounts()}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </main> 
+        );
+    }
+
+}
+export default App;
