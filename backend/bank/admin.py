6eda0d8b90b556289789f50e1d11a320c4554e25
deidra <deidraann22@gmail.com>
2019-12-30 13:13:53 -0800

    bugfix: small change to see if I can get my app to deploy

--- backend/bank/admin.py
+++ backend/bank/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 from .models import Branch, Client, Product, Account
 
-class BranchAdmin(admin.ModelAdmin):
-    list_display = ('branch_name', 'location_city')
-admin.site.register(Branch, BranchAdmin)
+admin.site.register(
+    (Branch,
+    Client,
+    Account,
+    Product)
+)
 
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('client_name', 'client_email', 'connect_to_branch')
-admin.site.register(Client, ClientAdmin)
+# class BranchAdmin(admin.ModelAdmin):
+#     list_display = ('branch_name', 'location_city')
+# admin.site.register(Branch, BranchAdmin)
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('default_account_types')
-admin.site.register(Product, ProductAdmin)
+# class ClientAdmin(admin.ModelAdmin):
+#     list_display = ('client_name', 'client_email', 'connect_to_branch')
+# admin.site.register(Client, ClientAdmin)
 
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ('connect_to_client','account_id')
-admin.site.register(Account, AccountAdmin)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('default_account_types')
+# admin.site.register(Product, ProductAdmin)
+
+# class AccountAdmin(admin.ModelAdmin):
+#     list_display = ('connect_to_client','account_id')
+# admin.site.register(Account, AccountAdmin)
