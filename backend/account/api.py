8674b8ee61e4b75f8d3607905965f40c3a8ce39f
deidra <deidraann22@gmail.com>
2020-01-14 16:26:31 -0800

    bugfix forgot import

--- backend/account/api.py
+++ backend/account/api.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer, LoginSerializer, GroupSerializer, PermissionSerializer
+from django.contrib.auth.models import User, Group
 
 class GroupAPI(generics.GenericAPIView):
     serializer_class = GroupSerializer
