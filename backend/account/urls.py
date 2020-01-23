cbddb1b92d4026fa7e16c19f9e9ee377b3f54f51
deidra <deidraann22@gmail.com>
2020-01-23 11:21:19 -0800

    removed: api/ from my urls

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -3,13 +3,13 @@ from .api import RegisterAPI, LoginAPI, UserAPI, PermissionAPI, AllPermissionsAP
 from knox import views as knox_views
 
 urlpatterns = [
-    path('api/auth', include('knox.urls')),
-    path('api/auth/register', RegisterAPI.as_view()),
-    path('api/auth/login', LoginAPI.as_view()),
-    path('api/auth/user', UserAPI.as_view()),
-    path('api/auth/permissions', PermissionAPI.as_view()),
-    path('api/auth/all-permissions', AllPermissionsAPI.as_view()),
-    path('api/auth/groups', GroupAPI.as_view()),
-    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('auth', include('knox.urls')),
+    path('auth/register', RegisterAPI.as_view()),
+    path('auth/login', LoginAPI.as_view()),
+    path('auth/user', UserAPI.as_view()),
+    path('auth/permissions', PermissionAPI.as_view()),
+    path('auth/all-permissions', AllPermissionsAPI.as_view()),
+    path('auth/groups', GroupAPI.as_view()),
+    path('auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
     path('auth/passwords', PasswordAPIView.as_view(), name="password-detail")
 ]
